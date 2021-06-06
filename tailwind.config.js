0f62c2b41ca56e30846885245d3e4a46c1ec9642
Snehanshu <hello@snehanshu.com>
2021-06-06 21:47:37 +0530

    setup project with lint and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      visibility: ['group-hover'],
+      display: ['responsive', 'group-hover'],
+    },
+  },
+  plugins: [],
+};
